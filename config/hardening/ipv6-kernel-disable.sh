f7da7f40063f36f37c7a710a49e3848757198d89
Tomas <tomas+github@libre.nz>
2018-02-12 16:24:37 +1300

    Update ipv6-kernel-disable.sh

--- config/hardening/ipv6-kernel-disable.sh
+++ config/hardening/ipv6-kernel-disable.sh
@@ -1,1 +1,11 @@
 
+#!/bin/sh
+#
+# Script: ipv6-kernel-disable.sh (system-hardening)
+# Description:Hardening - Disables IPv6 using kernel arguments
+# License: GPLv2
+########################################
+# Disable IPv6 from Kernel
+########################################
+/sbin/grubby --update-kernel=ALL --args="ipv6.disable=1"
+/usr/bin/sed -i "s/quiet/quiet ipv6.disable=1" /etc/default/grub
